c578efc281a353828ac3ded0e276572ef2d198cb
javalenciab90 <javalenciab90@gmail.com>
2020-09-17 19:05:25 -0500

    debug service comunication with Timber Library

--- app/src/main/java/com/java90/runway/services/TrackingService.kt
+++ app/src/main/java/com/java90/runway/services/TrackingService.kt
@@ -1,4 +1,33 @@
 package com.java90.runway.services
 
-class TrackingService {
+import android.content.Intent
+import androidx.lifecycle.LifecycleService
+import com.java90.runway.other.Constants.ACTION_PAUSE_SERVICE
+import com.java90.runway.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.java90.runway.other.Constants.ACTION_STOP_SERVICE
+import timber.log.Timber
+
+/*
+    Connection between Service and Activity
+*/
+
+class TrackingService : LifecycleService() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        intent?.let {
+            when(it.action) {
+                ACTION_START_OR_RESUME_SERVICE -> {
+                    Timber.d("Start or resumed service")
+                }
+                ACTION_PAUSE_SERVICE -> {
+                    Timber.d("Pause service")
+                }
+                ACTION_STOP_SERVICE -> {
+                    Timber.d("Stop service")
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
 }
