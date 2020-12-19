1324852c732951b84ecb53cb6a3afd9b40d49ae2
Jaime Valencia <javalenciab90@gmail.com>
2020-12-19 15:42:26 -0500

    Total Run Statitcs & SettingsFragment

--- app/src/main/java/com/java90/runway/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/java90/runway/ui/fragments/StatisticsFragment.kt
@@ -2,12 +2,47 @@ package com.java90.runway.ui.fragments
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.java90.runway.R
-import com.java90.runway.ui.viewmodels.MainViewModel
+import com.java90.runway.other.TrackingUtility
+import com.java90.runway.ui.viewmodels.StatisticsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_statistics.*
+import kotlin.math.round
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: StatisticsViewModel by viewModels()
+
+    private fun subscribeToObservers() {
+        viewModel.totalTImeRun.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val totalTimeRun = TrackingUtility.getFormattedStopWatchTime(it)
+                tvTotalTime.text = totalTimeRun
+            }
+        })
+        viewModel.totalDistance.observe(viewLifecycleOwner, Observer {
+            it.let {
+                val km = it/1000f
+                val totalDistance = round(km*10f) / 10f
+                val totalDistanceString = "${totalDistance}km"
+                tvTotalDistance.text = totalDistanceString
+            }
+        })
+        viewModel.totalAvgSpeed.observe(viewLifecycleOwner, Observer{
+            it?.let{
+                val avgSpeed = round(it*10f) / 10f
+                val avgSpeedString = "${avgSpeed}km/h"
+                tvAverageSpeed.text = avgSpeedString
+            }
+        })
+        viewModel.totalCaloriesBurned.observe(viewLifecycleOwner, Observer {
+            it?.let{
+                val totalCalories = "${it}kcal"
+                tvTotalCalories.text = totalCalories
+            }
+        })
+    }
+
 }
--- app/src/main/java/com/java90/runway/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/java90/runway/ui/viewmodels/StatisticsViewModel.kt
@@ -7,4 +7,10 @@ import com.java90.runway.repositories.MainRepository
 class StatisticsViewModel
     @ViewModelInject constructor(private val mainRepository: MainRepository) : ViewModel() {
 
+    val totalTImeRun = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+    val totalAvgSpeed = mainRepository.getTotalAvgSpeed()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
 }
